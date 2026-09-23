ab21a57eb4bedc04cec62fd2683f87206b067e0a
subodhisawake <subodhbhyri811@gmail.com>
2026-09-22 21:19:11 -0600

    Add Vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import Analytics from '@vercel/analytics';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+
+// Initialize Vercel Analytics
+Analytics.inject();
+
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
